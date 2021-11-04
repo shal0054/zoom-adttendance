f4aeff044c8cf3d032f6e63cfeac2db3d8f35d72
Karim Shaloh <shal0054@algonquinlive.com>
2021-11-04 18:40:55 -0300

    can loop and all csv file in all subdirectories of rootdir and generate unpopulated final df

--- main.py
+++ main.py
@@ -7,41 +7,56 @@
 # (installing pandas should automatically install numpy)
 import pandas as pd  # pip install pandas
 import numpy as np   # pip install numpy
+import os
 
-# read csv file
-data = pd.read_csv('zoomus_meeting_report_92152812677-2.csv')
+# Create empty dataframe that will populate with overall attendance
+attendances_df = pd.DataFrame()
 
-# Get the date
-date = data.values[0][2].split(' ')[0]
+# Input your root directory here
+rootdir = '/Users/karimshaloh/Documents/VSCODE/zoom_attendance'
 
-# Get class duration and set a cutoff time
-max_time = data['Duration(Minutes)'].max()
-cutoff_time = max_time * 0.5
+for subdir, dirs, files in os.walk(rootdir):
+    for file in files:
+        if file.endswith(".csv"):
+            filepath = os.path.join(subdir, file)
+            print(filepath)
 
-# Create new column and calculate total time present in class
-data['Total Time Present'] = data.groupby('User Name')[
-    'Duration(Minutes)'].transform('sum')
+            # read csv file
+            data = pd.read_csv(filepath)
 
-# drop unnecessary columns
-data.drop('User Email', inplace=True, axis=1)
-data.drop('Attentiveness Score', inplace=True, axis=1)
-data.drop('Duration(Minutes)', inplace=True, axis=1)
+            # Get the date
+            date = data.values[0][2].split(' ')[0]
 
-# Remove duplicates
-data = data.drop_duplicates(subset='User Name')
+            # Get class duration and set a cutoff time
+            max_time = data['Duration(Minutes)'].max()
+            cutoff_time = max_time * 0.5
 
-# Drop date and time columns
-data.drop('Join time', inplace=True, axis=1)
-data.drop('Leave time', inplace=True, axis=1)
+            # Create new column and calculate total time present in class
+            data['Total Time Present'] = data.groupby('User Name')[
+                'Duration(Minutes)'].transform('sum')
 
-# Add present column and populate it
-# Student is counted as present if there were present for at least half of the class
-# Set the present cutoff_time on line 67
-data["Present"] = np.where(data['Total Time Present'] >= cutoff_time, 1, 0)
-print(data['Present'])
+            # drop unnecessary columns
+            data.drop('User Email', inplace=True, axis=1)
+            data.drop('Attentiveness Score', inplace=True, axis=1)
+            data.drop('Duration(Minutes)', inplace=True, axis=1)
 
-# Create final DataFrame that holds all attendances results
-attendances_df = pd.DataFrame(
-    np.array(data["User Name"]), columns=['Student Name'])
-attendances_df[date] = data["Present"].to_numpy()
-print(attendances_df)
+            # Remove duplicates
+            data = data.drop_duplicates(subset='User Name')
+
+            # Drop date and time columns
+            data.drop('Join time', inplace=True, axis=1)
+            data.drop('Leave time', inplace=True, axis=1)
+
+            # Add present column and populate it
+            # Student is counted as present if there were present for at least half of the class
+            # Set the present cutoff_time on line 67
+            data["Present"] = np.where(
+                data['Total Time Present'] >= cutoff_time, 1, 0)
+
+            # Create final DataFrame that holds all attendances results
+            if attendances_df.empty == True:
+                attendances_df['Student Name'] = data["User Name"]
+                attendances_df[date] = data["Present"].to_numpy()
+                print(attendances_df)
+            else:
+                print("need to add to it")
